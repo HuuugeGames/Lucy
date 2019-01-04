127d73422e02c112ba3d0403122cb85fd7a56006
Bartosz Szreder <bartosz.szreder@huuugegames.com>
2019-01-04 11:25:18 +0100

    Move assert a bit further down.

--- src/Lucy/AST.cpp
+++ src/Lucy/AST.cpp
@@ -14,9 +14,9 @@ const std::string & LValue::resolveName() const
 	if (m_type == LValue::Type::Name)
 		return name();
 
-	assert(m_tableExpr->type() == Node::Type::LValue);
 	if (m_tableExpr->type() != Node::Type::LValue) {
 		LOG(Logger::Warning, location() << " : nontrivial (" << toUnderlying(m_tableExpr->type()) << ") expression for table name\n");
+		assert(m_tableExpr->type() == Node::Type::LValue);
 		return EmptyString;
 	}
 
