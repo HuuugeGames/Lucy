3520b39a1744895bd3651f28d85c8d2877a28ce4
Bartosz Szreder <bartosz.szreder@huuugegames.com>
2019-01-22 13:09:58 +0100

    Initialize all bits to 0.

--- src/Lucy/Bitfield.hpp
+++ src/Lucy/Bitfield.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
+#include <algorithm>
 #include <array>
 
 template <unsigned Size, typename Base = uint8_t>
 class Bitfield {
 public:
+	Bitfield() { std::fill(m_bits.begin(), m_bits.end(), static_cast<Base>(0)); }
 	unsigned get(unsigned idx) const { return (m_bits[index(idx)] >> bit(idx)) & 1; }
 	void set(unsigned idx) { m_bits[index(idx)] |= (1 << bit(idx)); }
 	void unset(unsigned idx) { m_bits[index(idx)] &= ~(1 << bit(idx)); }
