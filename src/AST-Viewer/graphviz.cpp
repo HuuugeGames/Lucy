69485bbf39ae88c6bc6b9898024d0c7fe8e754bc
Bartosz Szreder <bartosz.szreder@huuugegames.com>
2019-05-24 17:16:05 +0200

    Initial AST Viewer implementation.

---
+++ src/AST-Viewer/graphviz.cpp
@@ -0,0 +1,219 @@
+#include <array>
+#include <cgraph.h>
+#include <cstdio>
+#include <gvc.h>
+#include <sstream>
+
+#include "graphviz.hpp"
+
+#include "Lucy/AST.hpp"
+
+namespace {
+
+class GraphContext {
+public:
+	GraphContext(const AST::Chunk &root);
+	GraphContext(const GraphContext &) = delete;
+	GraphContext(GraphContext &&) = delete;
+	GraphContext & operator = (const GraphContext &) = delete;
+	GraphContext & operator = (GraphContext &&) = delete;
+	~GraphContext();
+
+	Agraph_t * graph() const { return m_graph; }
+	void reload(std::vector <Node> &nodes, std::vector <Edge> &edges, const char *data);
+
+private:
+	struct GraphNode {
+		GraphNode(const char *label, const yy::location &loc, Agnode_t *node)
+			: label{label}, loc{loc}, node{node} {}
+
+		std::string label;
+		yy::location loc;
+		Agnode_t *node = nullptr;
+	};
+
+	unsigned createNode(const char *label, const yy::location &loc)
+	{
+		const unsigned nodeIdx = m_graphNodes.size();
+		char buff[25];
+		sprintf(buff, "%u", nodeIdx);
+
+		Agnode_t *node = agnode(m_graph, buff, 1);
+		m_graphNodes.emplace_back(label, loc, node);
+		agset(node, const_cast<char *>("label"), m_graphNodes.back().label.data());
+
+		agset(node, const_cast<char *>("comment"), buff);
+		return nodeIdx;
+	}
+
+	void pushNode(const char *label, const yy::location &loc)
+	{
+		const unsigned parentIdx = m_nodeStack.back(), curIdx = createNode(label, loc);
+
+		m_nodeStack.push_back(curIdx);
+		m_edges.push_back(Edge{parentIdx, curIdx});
+		agedge(m_graph, m_graphNodes[parentIdx].node, m_graphNodes[curIdx].node, nullptr, 1);
+	}
+
+	void popNode()
+	{
+		m_nodeStack.pop_back();
+	}
+
+	void walk(const AST::Assignment &assignment);
+	void walk(const AST::BinOp &binOp);
+	void walk(const AST::ExprList &exprList);
+	void walk(const AST::LValue &lval);
+	void walk(const AST::Node &node);
+	void walk(const AST::UnOp &unOp);
+	void walk(const AST::VarList &varList);
+
+	Agraph_t *m_graph = nullptr;
+	std::vector <GraphNode> m_graphNodes;
+	std::vector <Edge> m_edges;
+	std::vector <unsigned> m_nodeStack;
+};
+
+GraphContext::GraphContext(const AST::Chunk &root)
+{
+	m_graph = agopen(const_cast<char *>("AST"), Agstrictdirected, nullptr);
+
+	agattr(m_graph, AGNODE, const_cast<char *>("comment"), const_cast<char *>(""));
+
+	m_nodeStack.push_back(createNode("Chunk", root.location()));
+	for (const auto &n : root.children())
+		walk(*n);
+}
+
+GraphContext::~GraphContext()
+{
+	agclose(m_graph);
+}
+
+void GraphContext::reload(std::vector <Node> &nodes, std::vector <Edge> &edges, const char *data)
+{
+	std::unique_ptr <Agraph_t, decltype(&agclose)> graph{agmemread(data), &agclose};
+	assert(graph);
+	nodes.resize(agnnodes(graph.get()));
+	Agnode_t *node = agfstnode(graph.get());
+
+	std::string tmp;
+	Rect boundingBox;
+
+	std::istringstream is{agget(graph.get(), const_cast<char *>("bb"))};
+	is >> boundingBox.x;
+	is.ignore();
+	is >> boundingBox.y;
+	is.ignore();
+	is >> boundingBox.width;
+	is.ignore();
+	is >> boundingBox.height;
+
+	boundingBox.width -= boundingBox.x;
+	boundingBox.height -= boundingBox.y;
+
+	while (node) {
+		unsigned nodeIdx = atol(agget(node, const_cast<char *>("comment")));
+		Node &rn = nodes[nodeIdx];
+		rn.codeLocation = m_graphNodes[nodeIdx].loc;
+		rn.label = agget(node, const_cast<char *>("label"));
+		rn.r.width = atof(agget(node, const_cast<char *>("width")));
+		rn.r.height = atof(agget(node, const_cast<char *>("height")));
+
+		is.clear();
+		is.str(agget(node, const_cast<char *>("pos")));
+		is >> rn.r.x;
+		is.ignore();
+		is >> rn.r.y;
+		rn.r.y = boundingBox.height - rn.r.y;
+
+		node = agnxtnode(graph.get(), node);
+	}
+
+	edges = std::move(m_edges);
+}
+
+void GraphContext::walk(const AST::Assignment &assignment)
+{
+	pushNode("=", assignment.location());
+	walk(assignment.varList());
+	walk(assignment.exprList());
+	popNode();
+}
+
+void GraphContext::walk(const AST::BinOp &binOp)
+{
+	pushNode(binOp.toString(), binOp.location());
+	walk(binOp.left());
+	walk(binOp.right());
+	popNode();
+}
+
+void GraphContext::walk(const AST::ExprList &exprList)
+{
+	pushNode("ExprList", exprList.location());
+	for (const auto &e : exprList.exprs())
+		walk(*e);
+	popNode();
+}
+
+void GraphContext::walk(const AST::LValue &lval)
+{
+	pushNode(lval.resolveName().c_str(), lval.location());
+	popNode();
+}
+
+void GraphContext::walk(const AST::Node &node)
+{
+	#define SUBCASE(type) case AST::Node::Type::type: walk(static_cast<const AST::type &>(node)); break
+
+	switch (node.type().value()) {
+		SUBCASE(Assignment);
+		SUBCASE(BinOp);
+		SUBCASE(LValue);
+		SUBCASE(UnOp);
+		default:
+			pushNode(node.type(), node.location());
+			popNode();
+			break;
+	}
+
+	#undef SUBCASE
+}
+
+void GraphContext::walk(const AST::UnOp &unOp)
+{
+	pushNode(unOp.toString(), unOp.location());
+	walk(unOp.operand());
+	popNode();
+}
+
+void GraphContext::walk(const AST::VarList &varList)
+{
+	pushNode("VarList", varList.location());
+	for (const auto &v : varList.vars())
+		walk(*v);
+	popNode();
+}
+
+} //namespace
+
+void DotGraph::prepare(const AST::Chunk &root)
+{
+	static GVC_t *graphvizCtx = gvContext();
+
+	GraphContext ctx(root);
+
+	/*
+	 * Graphviz does not generate position information in the "layout"
+	 * stage, but in the "render" stage.
+	 */
+	gvLayout(graphvizCtx, ctx.graph(), "dot");
+
+	char *data;
+	unsigned length;
+	gvRenderData(graphvizCtx, ctx.graph(), "dot", &data, &length);
+	ctx.reload(m_nodes, m_edges, data);
+	gvFreeRenderData(data);
+	gvFreeLayout(graphvizCtx, ctx.graph());
+}
---
+++ src/AST-Viewer/main.cpp
@@ -0,0 +1,211 @@
+#include <GL/glew.h>
+#include <SDL.h>
+
+#include <cstdio>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#include "imgui.h"
+#include "imgui_impl_sdl.h"
+#include "imgui_impl_opengl3.h"
+
+#include "Lucy/Driver.hpp"
+#include "graphviz.hpp"
+
+struct ASTGenState {
+	ASTGenState() = default;
+	ASTGenState(const ASTGenState &) = delete;
+	ASTGenState(ASTGenState &&) = default;
+	ASTGenState & operator = (const ASTGenState &) = delete;
+	ASTGenState & operator = (ASTGenState &&) = default;
+	~ASTGenState() = default;
+
+	std::string code;
+	std::string errorLog;
+	DotGraph graph;
+	std::unique_ptr <AST::Chunk> astRoot;
+	bool closed = false;
+};
+
+int sourceEditCallback(ImGuiInputTextCallbackData *data)
+{
+	if (data->EventFlag == ImGuiInputTextFlags_CallbackResize) {
+		std::string *buff = static_cast<std::string *>(data->UserData);
+		buff->resize(data->BufTextLen);
+		data->Buf = buff->data();
+	}
+
+	return 0;
+}
+
+ASTGenState generateAST(const std::string &luaCode)
+{
+	std::istringstream ss{luaCode};
+	Driver d;
+	d.setInputStream(&ss);
+
+	std::ostringstream errorStream;
+	d.setErrorStream(&errorStream);
+
+	ASTGenState result;
+	result.code = luaCode;
+
+	if (d.parse() != 0) {
+		result.errorLog = errorStream.str();
+	} else {
+		result.astRoot = std::move(d.chunks()[0]);
+		result.graph.prepare(*result.astRoot);
+	}
+
+	return result;
+}
+
+std::ostream & operator << (std::ostream &os, const ImVec2 &vec)
+{
+	os << '[' << vec.x << ", " << vec.y << ']';
+	return os;
+}
+
+int main()
+{
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
+		fprintf(stderr, "Error: %s\n", SDL_GetError());
+		return -1;
+	}
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+	SDL_Window *window = SDL_CreateWindow("AST Viewer",
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720,
+		SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+	SDL_GLContext glCtx = SDL_GL_CreateContext(window);
+	SDL_GL_SetSwapInterval(1);
+
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize OpenGL loader!\n");
+		return 1;
+	}
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+
+	ImGui::StyleColorsDark();
+
+	ImGui_ImplSDL2_InitForOpenGL(window, glCtx);
+	ImGui_ImplOpenGL3_Init("#version 130");
+
+	bool show_demo_window = true;
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+	bool done = false;
+	std::string buffer = "a = b + c";
+
+	std::vector <ASTGenState> generatedAst;
+	std::string astError;
+
+	while (!done) {
+		SDL_Event event;
+		while (SDL_PollEvent(&event))
+		{
+			ImGui_ImplSDL2_ProcessEvent(&event);
+			if (event.type == SDL_QUIT)
+				done = true;
+			if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window))
+				done = true;
+		}
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplSDL2_NewFrame(window);
+		ImGui::NewFrame();
+
+		if (show_demo_window)
+			ImGui::ShowDemoWindow(&show_demo_window);
+
+		{
+			ImGui::Begin("AST Viewer");
+
+			ImGui::Text("Code:");
+			ImGui::InputTextMultiline("##source", buffer.data(), buffer.size() + 1,
+				ImVec2{-1.0f, ImGui::GetTextLineHeight() * 16},
+				ImGuiInputTextFlags_AllowTabInput | ImGuiInputTextFlags_CallbackResize,
+				sourceEditCallback, &buffer);
+
+			if (!astError.empty())
+				ImGui::Text(astError.data());
+
+			if (ImGui::Button("Generate AST")) {
+				std::cerr << "buffer: " << buffer << '\n';
+				auto ast = generateAST(buffer);
+				astError.clear();
+				if (!ast.errorLog.empty())
+					astError = std::move(ast.errorLog);
+				else if (!ast.astRoot->isEmpty())
+					generatedAst.emplace_back(std::move(ast));
+			}
+
+			for (auto &ast : generatedAst) {
+				if (!ast.closed) {
+					ImGui::PushID(&ast);
+					ImGui::Begin("view", &ast.closed);
+
+					auto *drawList = ImGui::GetWindowDrawList();
+					drawList->ChannelsSplit(2);
+
+					const auto &nodes = ast.graph.nodes();
+					std::vector <ImVec2> nodeCenter;
+
+					drawList->ChannelsSetCurrent(1);
+					for (const auto &n : nodes) {
+						ImGui::SetCursorPos(ImVec2{n.r.x, n.r.y});
+						ImGui::Button(n.label.c_str());
+						if (ImGui::IsItemHovered()) {
+							std::ostringstream ss;
+							ss << n.codeLocation;
+							ImGui::SetTooltip(ss.str().c_str());
+						}
+
+						auto topLeft = ImGui::GetItemRectMin();
+						auto bottomRight = ImGui::GetItemRectMax();
+						nodeCenter.push_back(ImVec2{(topLeft.x + bottomRight.x) * 0.5f, (topLeft.y + bottomRight.y) * 0.5f});
+					}
+
+					drawList->ChannelsSetCurrent(0);
+					for (const auto &e : ast.graph.edges())
+						drawList->AddLine(nodeCenter[e.u], nodeCenter[e.v], 0xFFFFFFFF, 1);
+
+					drawList->ChannelsMerge();
+					ImGui::End();
+					ImGui::PopID();
+				}
+			}
+
+			ImGui::End();
+		}
+
+		ImGui::Render();
+		SDL_GL_MakeCurrent(window, glCtx);
+		glViewport(0, 0, io.DisplaySize.x, io.DisplaySize.y);
+		glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+		glClear(GL_COLOR_BUFFER_BIT);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		SDL_GL_SwapWindow(window);
+	}
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplSDL2_Shutdown();
+	ImGui::DestroyContext();
+
+	SDL_GL_DeleteContext(glCtx);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
+}
